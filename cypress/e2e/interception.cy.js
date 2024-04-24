701de66fa8e565fdaddc6cf4374771db0fa0ace2
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-04-24 20:53:48 +0300

    task is done

--- cypress/e2e/interception.cy.js
+++ cypress/e2e/interception.cy.js
@@ -3,7 +3,7 @@ import userInterception from "../fixtures/userInterception.json";
 describe("interception", () => {
 
     it("interception user", () => {
-        cy.intercept('GET', '**/userInterception', userInterception);
+        cy.intercept('https://qauto.forstudy.space/api/users/profile',userInterception);
         cy.visit('https://qauto.forstudy.space/', {
             auth: {
 
@@ -15,6 +15,9 @@ describe("interception", () => {
        cy.get('#signinEmail').type('emir@ua.fm')
        cy.get('#signinPassword').type('Qwerty12345')
        cy.contains('Login').click()
-    //    cy.contains('Profile').click()
+       cy.get('.icon-profile').click()
+       cy.get('.profile_name').should("have.text","Polar Bear")
+       cy.get('li:nth-child(1)').should("have.text","11.11.2011")
+       cy.get('li').last().should("have.text","USA")
   })
 })
